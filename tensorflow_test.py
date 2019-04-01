923645dbb2b5792071949a957813d15ca4cb1a71
pcyho <37729011+pcyho@users.noreply.github.com>
2019-04-01 17:00:49 +0800

    nothing

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -4,7 +4,7 @@
 import tensorflow as tf
 
 # 定义输入和参数
-x = tf.constant([[0.7, 0.5]])
+x = tf.placeholder('float32', shape=(None, 2))
 w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
 w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
 
@@ -16,6 +16,6 @@ y = tf.matmul(a, w2)
 with tf.Session() as sess:
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
-    print(sess.run(y))
+    print(sess.run(y, feed_dict={x: [[0.7, 0.5], [0.2, 0.3], [0.3, 0.4], [0.4, 0.5]]}))
 
 # x *w1 *w2 ssssss
