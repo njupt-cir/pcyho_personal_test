92e9a53bbf59f476bc635ad53f42872ddbc3bf32
pcyho <37729011+pcyho@users.noreply.github.com>
2019-04-04 18:44:20 +0800

    AI_class_test

--- liner_ragiulation.py
+++ liner_ragiulation.py
@@ -36,11 +36,12 @@ plt.scatter(X, Y, label='data')
 plt.plot(X, Y_line, label='result')
 plt.plot(X, y_hat, label='ragiulation_line')
 # plt.plot(X, Y_line, X, y_hat, 'b--')
+
 plt.legend()
 plt.show()
 # save data
-df = pd.DataFrame(list(zip(X.T.tolist()[0], Y.T.tolist()[0], y_hat.T.tolist()[0],(y_hat-Y_line).T.tolist()[0])))
-df.columns = ['X', 'Y', 'y_hat','Y-y_hat']
+df = pd.DataFrame(list(zip(X.T.tolist()[0], Y.T.tolist()[0], y_hat.T.tolist()[0], (y_hat - Y_line).T.tolist()[0])))
+df.columns = ['X', 'Y', 'y_hat', 'Y-y_hat']
 df.to_csv('out.csv', encoding='gbk', index=False)
 # 读取数据
 Data = pd.read_csv('out.csv')
